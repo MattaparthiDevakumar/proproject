35e5a993973fd1b60c553949ff254f85e72b0f87
deva <deva@gmail.com>
2019-06-24 06:53:43 +0000

    commited

---
+++ src/test/java/com/procode/examples/HelloWorldTest.java
@@ -0,0 +1,22 @@
+package com.procode.examples;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	private HelloWorld subject;
+
+	@Before
+	public void setup() {
+		subject = new HelloWorld();
+	}
+	
+	@Test
+	public void testGetMessage() {
+		assertEquals("Hello World!", subject.getMessage(false));
+	}
+	
+}
